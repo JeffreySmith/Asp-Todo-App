03820eee4eb793e630a43ab655ab8cb76219b5f5
Jeffrey Smith <jeffrey.smith7@gmail.com>
2023-11-30 08:58:42 -0500

    Added the basic todo model

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -11,6 +11,12 @@ public class Todo
     public string? Contents { get; set; }
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
+    public int? Priority { get; set; }
+    
+    public List<String>? Tags { get; set; }
+    
+    public bool Share { get; set; }
+    public string? Uuid { get; set; }
     
     [ForeignKey("IdentityUser")]
     public int UserId { get; set; }
